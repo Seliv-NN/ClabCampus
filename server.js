56b64f0e5a4c8a9f386146876221c39aa90e9c76
Seliv-NN <nat325@mail.ru>
2025-04-25 22:57:13 +0300

    server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ function requireTeacher(req, res, next) {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 
 // API endpoints
 
